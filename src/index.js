0e5b48def667d2f739066a49d90da4ef9bfcda44
Malina <stark7415963@gmail.com>
2020-10-02 03:41:38 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = 0) {
+  if ((array.length) > 0)
+  {
+    let minElement = array[0];
+    for (let i = 1; i < (array.length); i++)
+    {
+      if (array[i] < minElement)
+      {
+        minElement = array[i];
+      }
+    }
+    return minElement;
+  }
+  else
+  {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = 0) {
+  if ((array.length) > 0)
+  {
+    let maxElement = array[0];
+    for (let i = 1; i < (array.length); i++)
+    {
+      if (array[i] > maxElement)
+      {
+        maxElement = array[i];
+      }
+    }
+    return maxElement;
+  }
+  else
+  {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = 0) {
+  let sum = 0;
+  let counter = 0;
+  if ((array.length) > 0)
+  {
+    for (let i = 0; i < (array.length); i++)
+    {
+      counter += 1;
+      sum += array[i];
+    }
+    return (sum / counter);
+  }
+  else
+  {
+    return 0;
+  }
 }
